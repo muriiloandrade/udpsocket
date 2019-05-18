ccbfa3f17ca6681446758329a833412760edb9a3
Murilo Andrade <muriloandradealmeida@gmail.com>
2019-05-18 00:28:34 -0300

    Quando não digitar a porta, uma porta é dinamicamente alocada ao socket.

--- UDPServer.java
+++ UDPServer.java
@@ -1,7 +1,8 @@
 import java.io.IOException;
+import java.net.SocketException;
+import java.util.InputMismatchException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.SocketException;
 import java.util.Scanner;
 
 public class UDPServer {
@@ -13,33 +14,34 @@ public class UDPServer {
     System.out.print("Informar porta? [S/N]: ");
     String flag = input.next();
 
-    if(flag.equalsIgnoreCase("s")) {
-      try {
+    try {
+      if(flag.equalsIgnoreCase("s")) {
         System.out.print("Digite o número desejado para a porta: ");
         porta = input.nextInt();
-      } catch (Exception e) {
-        System.err.println("Erro ao inserir a porta: " + e.getMessage());
+        socket = new DatagramSocket(porta);
       }
-    }
-    else {
-      porta = 6789;
-    }
-
-    System.out.printf("Servidor online e aguardando por cliente na porta %d!\n", porta);
-    
-    try {
-      socket = new DatagramSocket(porta);
+      else{
+        socket = new DatagramSocket();
+      }
+      
+      System.out.printf("Servidor online e aguardando por cliente na porta %d!\n", socket.getLocalPort());
+      
       byte[] buffer = new byte[1024];
       while (true) {
         DatagramPacket pctVeio = new DatagramPacket(buffer, buffer.length);
         socket.receive(pctVeio);
         System.out.println("Requisição recebida do cliente: " + pctVeio.getAddress());
+        
+        //TODO: Adicionar a thread numa lista, e quando a thread morrer, retirá-la da lista 
+        
         new UDPServerThread(pctVeio).start();
       }
     } catch (SocketException e) { 
       System.err.println("Erro no Socket: " + e.getMessage()); 
     } catch (IOException e) { 
       System.err.println("Erro de IO: " + e.getMessage()); 
-    } 
+    } catch (InputMismatchException e) {
+      System.err.println("Erro ao inserir a porta: " + e.getMessage());
+    }
   }
 }
